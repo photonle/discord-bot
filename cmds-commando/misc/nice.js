01ed8e77b2574487a412af3bf8619f08249669e3
John Internet <jonjon1234.github@gmail.com>
2019-02-02 22:58:56 +0000

    Was it really because it had an empty args table?

--- cmds-commando/misc/nice.js
+++ cmds-commando/misc/nice.js
@@ -3,15 +3,15 @@ const Command = require("discord.js-commando").Command
 module.exports = class NiceCommand extends Command {
 	constructor(client) {
 		super(client, {
-			name: 'debugs',
+			name: 'nice',
 			group: 'util',
-			memberName: 'debugs',
-			description: 'Output debug information such as uptime, API latency and version.',
+			memberName: 'nice',
+			description: '*nice*',
 		});
 	}
 
 	async run(msg, args){
-		console.log("running")
-		// return msg.channel.send("https://cdn.discordapp.com/attachments/539928445984178201/541373345644544020/weflip-thumbsup_904.png")
+		// console.log("running")
+		return msg.channel.send("https://cdn.discordapp.com/attachments/539928445984178201/541373345644544020/weflip-thumbsup_904.png")
 	}
 };
