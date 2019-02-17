53d2bec63acac5815ebdb3392f900585bc2155db
John Internet <jonjon1234.github@gmail.com>
2019-02-17 17:15:17 +0000

    Added double line spacing for debug info.

--- cmds-commando/debug/debug.js
+++ cmds-commando/debug/debug.js
@@ -17,7 +17,7 @@ module.exports = class DebugCommand extends Command {
 		let rich = new RichEmbed()
 
 		rich.setAuthor("Doctor Internet", msg.client.users.get("239031520587808769").avatarURL, "https://limelightgaming.net/forums/user-2746.html")
-		rich.setDescription(`A bot by Doctor Internet\nDebug Information`)
+		rich.setDescription(`A bot by Doctor Internet\n\nDebug Information`)
 
 		rich.addField("Version", `${pkg.name}@${pkg.version}`, true)
 		rich.addField("Uptime", Moment.duration(process.uptime(), 's').humanize(), true)
