4676925cd2cb0cbc51a71b2d8f3d7d79787af765
John Internet <jonjon1234.github@gmail.com>
2019-02-25 15:45:58 +0000

    dont delete msg for !nice

--- cmds-commando/misc/nice.js
+++ cmds-commando/misc/nice.js
@@ -1,4 +1,4 @@
-const Command = require("discord.js-commando").Command
+const {Command} = require("discord.js-commando")
 const {Attachment} = require("discord.js")
 
 module.exports = class NiceCommand extends Command {
@@ -18,9 +18,5 @@ module.exports = class NiceCommand extends Command {
 	 * @param {boolean} _ If the incoming message is from a pattern match.
 	 * @returns {Promise<Message>}
 	 */
-	async run(msg, args, _){
-		await msg.delete()
-		return msg.channel.send(new Attachment("https://cdn.discordapp.com/attachments/539928445984178201/541373345644544020/weflip-thumbsup_904.png"))
-
-	}
+	async run(msg, args, _){return msg.say(new Attachment("https://cdn.discordapp.com/attachments/539928445984178201/541373345644544020/weflip-thumbsup_904.png"))}
 }
