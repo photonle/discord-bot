7d74d7bf79359b780ed2729f4a6da6e556c4a4e2
John Internet <jonjon1234.github@gmail.com>
2019-06-03 22:35:39 +0100

    Add component commands.

---
+++ cmds/photon/component.js
@@ -0,0 +1,35 @@
+const sqlite = require("sqlite")
+let db
+const SQL = require('sql-template-strings')
+const Command = require("discord.js-commando").Command
+
+module.exports = class ComponentCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'component',
+			group: 'util',
+			memberName: 'component',
+			description: 'Search for component names used in photon addons.',
+			args: [{
+				key: 'path',
+				label: 'Component Name',
+				prompt: 'Enter the name to search.',
+				type: 'string',
+			}]
+		})
+	}
+
+	async run(msg, args, _){
+		msg.say(`Searching for ${args.path} in addons.`)
+		let matches = await db.all(SQL`SELECT cname as path, COUNT(*) as count FROM components WHERE cname = ${args.path} GROUP BY cname`)
+
+		if (matches.length === 0){
+			return msg.say("I haven't seen that component name before.")
+		} else {
+			return Promise.all(matches.map(x => msg.say(`\`${x.path.replace(/`/, '\\`')}\` has been used in ${x.count} ${x.count === 1 ? 'addon' : 'addons'} that I've seen.`)))
+		}
+	}
+}
+
+async function main(){db = await sqlite.open("/app/photon.read.db")}
+main()
---
+++ cmds/photon/components.js
@@ -0,0 +1,43 @@
+const sqlite = require("sqlite")
+let db
+const SQL = require('sql-template-strings')
+const Command = require("discord.js-commando").Command
+const Embed = require('discord.js').RichEmbed
+
+module.exports = class ComponentCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'components',
+			group: 'util',
+			memberName: 'components',
+			description: 'Advanced search for components used in photon addons.',
+			args: [{
+				key: 'path',
+				label: 'Search Path',
+				prompt: 'Enter the path to search.',
+				type: 'string',
+			}]
+		})
+	}
+
+	async run(msg, args, _){
+		msg.say(`Searching for ${args.path} in addons.`)
+		let matches = await db.all(SQL`SELECT cname as path, COUNT(*) as count FROM cars WHERE cname = ${args.path} GROUP BY cname`)
+
+		matches = await Promise.all(matches.map(async x => {return {path: x.path, data: await db.all(SQL`SELECT cname as path, owner, name, sid, sname FROM components INNER JOIN addons on cars.owner = addons.wsid INNER JOIN authors ON addons.author = authors.sid WHERE cname = ${x.path}`)}}))
+		matches = matches.map(x => {
+			let y = new Embed()
+			let i = 1
+			y.setAuthor(`Component Report: ${x.path}`)
+			for (let addon of x.data){
+				y.addField(`Addon ${i++}`, `[${addon.name.replace(/([\[\]])/g, '\$1')}](https://steamcommunity.com/sharedfiles/filedetails/?id=${addon.owner}) by [${addon.sname.replace(/([\[\]])/g, '\$1')}](https://steamcommunity.com/profiles/${addon.sid})`)
+			}
+			return y
+		})
+		return Promise.all(matches.map(x => msg.say(x)))
+		// msg.say(`${matches[0].path}: ${matches[0].data}`)
+	}
+}
+
+async function main(){db = await sqlite.open("/app/photon.read.db")}
+main()
