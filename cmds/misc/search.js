2a913b7a7be6c48ef93faf013a2b25e70091d956
John Internet <jonjon1234.github@gmail.com>
2019-06-03 20:35:26 +0100

    Fixed search docs, removed promise library. Return correct type.

--- cmds/misc/search.js
+++ cmds/misc/search.js
@@ -1,7 +1,4 @@
-const Promise = require("promise")
-const Command = require("discord.js-commando").Command
-
-module.exports = class PurgeCommand extends Command {
+module.exports = class SearchCommand extends Command {
 	constructor(client) {
 		super(client, {
 			name: 'search',
@@ -20,16 +17,15 @@ module.exports = class PurgeCommand extends Command {
 				},
 			]
 		})
-
-		/**
-		 * @param {CommandMessage} msg The incoming message.
-		 * @param {Object|string|Array<string>} args The command arguments.
-		 * @param {boolean} _ If the incoming message is from a pattern match.
-		 * @returns {Promise<Message>}
-		 */
 		this.url = "https://lmgtfy.com/?q="
 	}
 
+	/**
+	 * @param {CommandMessage} msg The incoming message.
+	 * @param {Object|string|Array<string>} args The command arguments.
+	 * @param {boolean} _ If the incoming message is from a pattern match.
+	 * @returns {Promise<Message>}
+	 */
 	async run(msg, args, _){
 		if (args.term === '^'){
 			let msgs = await msg.channel.fetchMessages({limit: 1, before: msg.id})
@@ -37,6 +33,8 @@ module.exports = class PurgeCommand extends Command {
 		}
 
 		let query = encodeURI(args.term.toString())
-		msg.reply(`<${this.url}${query}>`)
+		return msg.reply(`<${this.url}${query}>`)
 	}
 }
+
+const Command = require("discord.js-commando").Command
