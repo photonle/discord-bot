e333c130d5c0c6d584499da234bbc44036c0a0e7
John Internet <jonjon1234.github@gmail.com>
2019-06-03 21:55:26 +0100

    Add path for simple path searching.

--- cmds/photon/path.js
+++ cmds/photon/path.js
@@ -3,7 +3,7 @@ let db
 const SQL = require('sql-template-strings')
 const Command = require("discord.js-commando").Command
 
-module.exports = class GLuaCommand extends Command {
+module.exports = class PathCommand extends Command {
 	constructor(client) {
 		super(client, {
 			name: 'path',
@@ -20,12 +20,31 @@ module.exports = class GLuaCommand extends Command {
 	}
 
 	async run(msg, args, _){
-		let matches = await db.all(SQL`SELECT * FROM files WHERE path = ${args.path}`)
-		msg.say(` ${matches}`)
+		args.path = args.path.replace(/\//, '\\')
+		msg.say(`Searching for ${args.path.replace(/\\/g, '/')} in addons.`)
+
+		let matches = []
+		if (args.path.startsWith("lua")){
+			// Full Path
+			matches = await db.all(SQL`SELECT path, COUNT(*) as count FROM files WHERE path = ${args.path} GROUP BY path`)
+		} else if (args.path.endsWith(".lua")){
+			// End Path
+			args.path = `%${args.path}`
+			matches = await db.all(SQL`SELECT path, COUNT(*) as count FROM files WHERE path LIKE ${args.path} GROUP BY path`)
+		} else {
+			// Search
+			args.path = `%${args.path}%`
+			matches = await db.all(SQL`SELECT path, COUNT(*) as count FROM files WHERE path LIKE ${args.path} GROUP BY path`)
+		}
+
+		if (matches.length === 0){
+			return msg.say("I haven't seen that path before.")
+		} else {
+			matches = matches.map(x => {x.path = x.path.replace(/\\/g, '/'); return x})
+			return Promise.all(matches.map(x => msg.say(`${x.path} has been used in ${x.count} addons that I've seen.`)))
+		}
 	}
 }
 
-async function main(){
-	db = await sqlite.open("/app/photon.read.db")
-}
+async function main(){db = await sqlite.open("/app/photon.read.db")}
 main()
