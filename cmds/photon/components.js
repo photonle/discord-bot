ff2cabab524fa75f1b76c1d86febf0524c3fda86
John Internet <jonjon1234.github@gmail.com>
2019-06-03 22:37:31 +0100

    Fix the other query.

--- cmds/photon/components.js
+++ cmds/photon/components.js
@@ -22,7 +22,7 @@ module.exports = class ComponentCommand extends Command {
 
 	async run(msg, args, _){
 		msg.say(`Searching for ${args.path} in addons.`)
-		let matches = await db.all(SQL`SELECT cname as path, COUNT(*) as count FROM cars WHERE cname = ${args.path} GROUP BY cname`)
+		let matches = await db.all(SQL`SELECT cname as path, COUNT(*) as count FROM components WHERE cname = ${args.path} GROUP BY cname`)
 
 		matches = await Promise.all(matches.map(async x => {return {path: x.path, data: await db.all(SQL`SELECT cname as path, owner, name, sid, sname FROM components INNER JOIN addons on components.owner = addons.wsid INNER JOIN authors ON addons.author = authors.sid WHERE cname = ${x.path}`)}}))
 		matches = matches.map(x => {
