d61fcaf28ee7c3a13bd955f8de832b258a3fa02e
John Internet <jonjon1234.github@gmail.com>
2019-07-07 16:57:05 +0100

    Fixed bug reports / feature requests and allowed the bot to have bugs. Fixes #5.

--- cmds/misc/bug.js
+++ cmds/misc/bug.js
@@ -6,15 +6,24 @@ module.exports = class SupportCommand extends Command {
 			name: 'bug',
 			group: 'util',
 			memberName: 'bug',
-			description: 'Found a bug?'
+			description: 'Found a bug?',
+			args: [{
+				key: 'where',
+				label: 'Bug Location',
+				prompt: 'Where was the bug seen?',
+				type: 'string',
+				default: 'photon'
+			}]
 		})
 	}
 
-	/**
-	 * @param {CommandMessage} msg The incoming message.
-	 * @param {Object|string|Array<string>} args The command arguments.
-	 * @param {boolean} _ If the incoming message is from a pattern match.
-	 * @returns {Promise<Message>}
-	 */
-	async run(msg, args, _){return msg.say(`Looks like you've found a bug in Photon! Head over to <https://github.com/photonle/Photon/issues/new?labels=bug&template=bug-report.md> and post a bug report.`)}
+	async run(msg, args, _){
+		switch (args.where){
+			default:
+			case 'photon':
+				return msg.say(`Looks like you've found a bug in Photon! Head over to <https://github.com/photonle/Photon/issues/new?labels=bug&template=bug-report.md> and post a bug report.`)
+			case 'bot':
+				return msg.say(`Looks like you've found a bug in me! Head over to <https://github.com/photonle/Photon-Bot/issues/new?labels=bug&template=bug-report.md> and post a bug report.`)
+		}
+	}
 }
--- cmds/misc/feature.js
+++ cmds/misc/feature.js
@@ -10,11 +10,13 @@ module.exports = class SupportCommand extends Command {
 		})
 	}
 
-	/**
-	 * @param {CommandMessage} msg The incoming message.
-	 * @param {Object|string|Array<string>} args The command arguments.
-	 * @param {boolean} _ If the incoming message is from a pattern match.
-	 * @returns {Promise<Message>}
-	 */
-	async run(msg, args, _){return msg.say(`Looks like you've suggested a good idea! Head over to <https://github.com/photonle/Photon/issues/new?labels=enhancement&template=feature-request.md> and post a feature request.`)}
+	async run(msg, args, _){
+		switch (args.where){
+			default:
+			case 'photon':
+				return msg.say(`That looks like a good idea! Head over to <https://github.com/photonle/Photon/issues/new?labels=feature&template=feature-request.md> and post a feature request!`)
+			case 'bot':
+				return msg.say(`That looks like a good idea! Head over to <https://github.com/photonle/Photon-Bot/issues/new?labels=feature&template=feature-request.md> and post a feature request!`)
+		}
+	}
 }
