a80abb025851dcf7ca05c6b05fb6cc7f8e3afcc4
John Internet <jonjon1234.github@gmail.com>
2019-07-07 17:20:22 +0100

    Rewrote !cars

--- cmds/photon/cars.js
+++ cmds/photon/cars.js
@@ -21,22 +21,37 @@ module.exports = class CarCommand extends Command {
 	}
 
 	async run(msg, args, _){
-		msg.say(`Searching for ${args.path} in addons.`)
-		let matches = await db.all(SQL`SELECT cname as path, COUNT(*) as count FROM cars WHERE cname = ${args.path} GROUP BY cname`)
-		if (matches.length == 0){return msg.say("I haven't seen that car name before.")}
+		let reply = msg.say(`Searching for \`${args.path.replace(/`/, '\`')}\` in addons.`)
 
-		matches = await Promise.all(matches.map(async x => {return {path: x.path, data: await db.all(SQL`SELECT cname as path, owner, name, CAST(sid AS TEXT) as sid, sname FROM cars INNER JOIN addons on cars.owner = addons.wsid INNER JOIN authors ON addons.author = authors.sid WHERE cname = ${x.path}`)}}))
-		matches = matches.map(x => {
-			let y = new Embed()
+		let matches = await db.all(SQL`SELECT cname as path, COUNT(*) as count FROM cars WHERE cname = ${args.path} GROUP BY cname`)
+
+		matches = matches.filter(x => x.count > 0)
+		if (matches.length === 0){return (await reply).edit("I haven't seen that car name before.")}
+
+		matches = await Promise.all(
+			matches.map(async x => {
+				return {
+					path: x.path,
+					data: await db.all(SQL`SELECT cname as path, owner, name, CAST(sid AS TEXT) as sid, sname FROM cars INNER JOIN addons on cars.owner = addons.wsid INNER JOIN authors ON addons.author = authors.sid WHERE cname = ${x.path}`)
+				}
+			})
+		)
+
+		let embeds = matches.map(match => {
+			let embed = new Embed()
 			let i = 1
-			y.setAuthor(`Vehicle Report: ${x.path}`)
-			for (let addon of x.data){
-				y.addField(`Addon ${i++}`, `[${addon.name.replace(/([\[\]])/g, '\$1')}](https://steamcommunity.com/sharedfiles/filedetails/?id=${addon.owner}) by [${addon.sname.replace(/([\[\]])/g, '\$1')}](https://steamcommunity.com/profiles/${addon.sid})`)
-			}
-			return y
+			embed.setAuthor(`Vehicle Report: ${x.path}`)
+			match.data.map(addon =>
+				y.addField(
+					`Addon ${i++}`,
+					`[${addon.name.replace(/([\[\]])/g, '\$1')}](https://steamcommunity.com/sharedfiles/filedetails/?id=${addon.owner}) by [${addon.sname.replace(/([\[\]])/g, '\$1')}](https://steamcommunity.com/profiles/${addon.sid})`
+				)
+			)
+			return embed
 		})
-		return Promise.all(matches.map(x => msg.say(x)))
-		// msg.say(`${matches[0].path}: ${matches[0].data}`)
+
+		reply = await reply
+		return Promise.all(matches.map(x => reply.say(x)))
 	}
 }
 
