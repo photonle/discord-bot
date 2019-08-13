a2abf01a16d02efbe66ac51edff1751eb8117069
John Internet <jonjon1234.github@gmail.com>
2019-08-13 11:10:48 +0100

    Add command to force a workshop update.

---
+++ cmds/photon/update.js
@@ -0,0 +1,29 @@
+const Command = require("discord.js-commando").Command
+
+module.exports = class CarCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'update',
+			group: 'util',
+			memberName: 'update',
+			description: 'Queue a workshop update job.',
+			args: [{
+				key: 'wsid',
+				label: 'Workshop ID',
+				prompt: 'Enter the workshop ID to search.',
+				type: 'string',
+			}]
+		})
+	}
+
+	async run(msg, args, _){
+		let {wsid} = args
+
+		let r = /\D/
+		while (wsid.match(r)){
+			wsid = wsid.replace(r, '')
+		}
+
+		return msg.say(`loading ${wsid}`)
+	}
+}
