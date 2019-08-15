065064e24cffa7ae5288cffaa4a2f98e018fe3c2
John Internet <jonjon1234.github@gmail.com>
2019-08-15 11:51:59 +0100

    Add test command and finder base.

---
+++ cmds/findercommand.js
@@ -0,0 +1,46 @@
+const SQL = require('sql-template-strings')
+const Command = require("../sqlcommand")
+
+module.exports = class FinderCommand extends Command {
+	constructor(client, data) {
+		super(client, data)
+
+		this.queryTable = "cars"
+		this.queryType = "vehicle"
+		this.finderType = "name"
+		this.finderName = "cname"
+	}
+
+	async run(msg, args, _){
+		args.path = `%${args.path}%`
+		let reply = msg.say(`Searching for \`${args.path.replace(/`/, '\`')}\` in addons.`)
+
+		let matches = await this.query(SQL`SELECT cname as path, COUNT(*) as count FROM cars WHERE cname LIKE ${args.path} GROUP BY cname`)
+		if (matches.length === 0){
+			return (await reply).edit("I haven't seen that vehicle name before.")
+		} else {
+			matches = matches.map(x => `\`${x.path.replace(/`/, '\\`')}\` has been used in ${x.count} ${x.count === 1 ? 'addon' : 'addons'} that I've seen.`).join("\n\n")
+			if (matches.length > 1800){
+				return (await reply).edit("Please be more specific.")
+			} else {
+				return (await reply).edit(`${matches}\n\nTo see which addons a vehicle is used in, run !${this.name}s {component}`)
+			}
+		}
+	}
+
+	 generateWhere(query, str){
+		if (this.queryTable === "files"){
+			if (str.startsWith("lua")){
+				return query
+					.append(`WHERE ${this.finderName} = `)
+					.append(SQL(str))
+			}
+			return query.append(`WHERE ${this.finderName} LIKE `)
+				.append(str.endsWith(".lua") ? SQL(`%${str}`) : SQL(`%${str}%`))
+		} else {
+			return query
+				.append(`WHERE ${this.finderName} LIKE `)
+				.append(SQL(`%${str}%`))
+		}
+	 }
+}
---
+++ cmds/photon/test.js
@@ -0,0 +1,43 @@
+const SQL = require('sql-template-strings')
+const Command = require("../findercommand.js")
+
+module.exports = class TestCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'test',
+			group: 'util',
+			memberName: 'test',
+			description: 'Search for vehicle names used in photon addons.',
+			args: [{
+				key: 'path',
+				label: 'Vehicle Name',
+				prompt: 'Enter the name to search.',
+				type: 'string',
+			}]
+		})
+
+		this.queryTable = "cars"
+		this.queryType = "vehicle"
+		this.finderType = "name"
+		this.finderName = "cname"
+	}
+
+	async run(msg, args, _){
+		let reply = msg.say(`Searching for ${this.queryTable} ${this.finderType} \`${args.path.replace(/`/, '\`')}\` in addons.`)
+
+		let query = SQL(`SELECT ${this.finderName} path, COUNT(*) count FROM ${this.queryTable}`)
+		query = this.generateWhere(query, args.path).append(`GROUP BY ${this.finderName}`)
+
+		let matches = await this.query(query)
+		if (matches.length === 0){
+			return (await reply).edit("I haven't seen that vehicle name before.")
+		} else {
+			matches = matches.map(x => `\`${x.path.replace(/`/, '\\`')}\` has been used in ${x.count} ${x.count === 1 ? 'addon' : 'addons'} that I've seen.`).join("\n\n")
+			if (matches.length > 1800){
+				return (await reply).edit("Please be more specific.")
+			} else {
+				return (await reply).edit(`${matches}\n\nTo see which addons a vehicle is used in, run !${this.name}s {component}`)
+			}
+		}
+	}
+}
