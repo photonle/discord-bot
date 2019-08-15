557ef1fedaf7dc55e0fcdfc8ee5eff4186db72ef
John Internet <jonjon1234.github@gmail.com>
2019-08-15 11:05:37 +0100

    Don't pass data if we're using an sqlstatement.

--- cmds/sqlcommand.js
+++ cmds/sqlcommand.js
@@ -1,8 +1,14 @@
 const Command = require("discord.js-commando").Command
+const SQLStatement = require("sql-template-strings")
 const pool = require('../libs/sql.js')
 
 module.exports = class SQLCommand extends Command {
 	async query(query, data = {}){
+		if (query instanceof SQLStatement){
+			let [rows, fields] = await pool.query(query)
+			return rows
+		}
+
 		let [rows, fields] = await pool.query(query, data)
 		return rows
 	}
