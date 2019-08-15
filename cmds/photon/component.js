e91b7d3505f6325efa98ff855998b5959bad5072
John Internet <jonjon1234.github@gmail.com>
2019-08-15 11:30:19 +0100

    Add a better err msg. Flatten the structure.

--- cmds/photon/component.js
+++ cmds/photon/component.js
@@ -18,20 +18,20 @@ module.exports = class ComponentCommand extends Command {
 	}
 
 	async run(msg, args, _){
+		args.path = `%${args.path}%`
 		let reply = msg.say(`Searching for \`${args.path.replace(/`/, '\`')}\` in addons.`)
 
-		args.path = `%${args.path}%`
 		let matches = await this.query(SQL`SELECT cname path, COUNT(*) count FROM components WHERE cname LIKE ${args.path} GROUP BY cname`)
 		if (matches.length === 0){
 			return (await reply).edit("I haven't seen that component name before.")
-		} else {
-			matches = matches.map(x => `\`${x.path.replace(/`/, '\\`')}\` has been used in ${x.count} ${x.count === 1 ? 'addon' : 'addons'} that I've seen.`).join("\n\n")
-			if (matches.length > 1800){
-				return (await reply).edit("Please be more specific.")
-			} else {
-				return (await reply).edit(matches)
-			}
 		}
+
+		matches = matches.map(x => `\`${x.path.replace(/`/, '\\`')}\` has been used in ${x.count} ${x.count === 1 ? 'addon' : 'addons'} that I've seen.`).join("\n\n")
+		if (matches.length > 1800){
+			return (await reply).edit("Please be more specific.")
+		}
+
+		return (await reply).edit(`${matches}\n\nTo see which addons a vehicle is used in, run !${this.name}s {component}`)
 	}
 }
 
