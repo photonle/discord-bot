f929a92435c43d6a18c97d6c68f011d1e7ee5675
John Internet <jonjon1234.github@gmail.com>
2019-08-15 11:26:09 +0100

    Handle sql statements.

--- cmds/sqlcommand.js
+++ cmds/sqlcommand.js
@@ -4,14 +4,8 @@ const pool = require('../libs/sql.js')
 
 module.exports = class SQLCommand extends Command {
 	async query(query, data = {}){
-		console.log(query)
-		console.log(query instanceof SQLStatement)
-		console.log(query.prototype.name)
-
 		if (query instanceof SQLStatement){
-			console.log(query.sql)
-			console.log(query.values)
-			let [rows, fields] = await pool.query(query.sql, query.values)
+			let [rows, fields] = await pool.query(query)
 			return rows
 		}
 
