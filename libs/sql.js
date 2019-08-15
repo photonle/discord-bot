6faca0a4e239bfe309a7eb90f494e05fe3d7e462
John Internet <jonjon1234.github@gmail.com>
2019-08-15 09:59:07 +0100

    Add SQL lib.

---
+++ libs/sql.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql2/promise')
+
+const pool = mysql.createPool({
+	host: 'db',
+	user: process.env.MYSQL_USER,
+	password: process.env.MYSQL_PASSWORD,
+	database: process.env.MYSQL_DATABASE,
+	waitForConnections: true,
+	connectionLimit: 10,
+	queueLimit: 0
+}).promise()
+return pool
