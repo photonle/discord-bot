ff302d6d1675d4f493b75e1e6f5f37f75f19e752
John Internet <jonjon1234.github@gmail.com>
2019-08-15 11:18:15 +0100

    log more stuff.

--- cmds/sqlcommand.js
+++ cmds/sqlcommand.js
@@ -4,6 +4,10 @@ const pool = require('../libs/sql.js')
 
 module.exports = class SQLCommand extends Command {
 	async query(query, data = {}){
+		console.log(query)
+		console.log(query instanceof SQLStatement)
+		console.log(query.prototype.name)
+
 		if (query instanceof SQLStatement){
 			console.log(query.sql)
 			console.log(query.values)
