d4f9d1d1b2de44aeaf8005c4afcc05c0153238af
John Internet <jonjon1234.github@gmail.com>
2019-08-18 20:11:16 +0100

    Add force argument to global updater.

--- cmds/photon/updateAll.js
+++ cmds/photon/updateAll.js
@@ -7,12 +7,19 @@ module.exports = class WorkshopUpdateCommand extends Command {
 			group: 'photon',
 			memberName: 'updateall',
 			description: 'Queue a global workshop update.',
-			ownerOnly: true
+			ownerOnly: true,
+			args: [{
+				key: 'force',
+				label: 'Force Update',
+				prompt: 'Enter if the update should be forced.',
+				type: 'boolean',
+				default: false
+			}]
 		})
 	}
 
 	async run(msg, args){
-		await this.queue('UpdateAllWorkshop')
+		await this.queue('UpdateAllWorkshop', args)
 		return msg.say(`Added bulk update job to the update queue.\nCheck <#611492538674839553> for status changes.`)
 	}
 }
