c34818c00911b4d2996c65c694d7eec428aa53b1
John Internet <jonjon1234.github@gmail.com>
2019-08-18 17:59:07 +0100

    Add update force option to the bot.

--- cmds/photon/update.js
+++ cmds/photon/update.js
@@ -12,19 +12,25 @@ module.exports = class WorkshopUpdateCommand extends Command {
 				label: 'Workshop ID',
 				prompt: 'Enter the workshop ID to search.',
 				type: 'string',
+			}, {
+				key: 'force',
+				label: 'Force Update',
+				prompt: 'Enter if the update should be forced.',
+				type: 'boolean',
+				default: false
 			}]
 		})
 	}
 
 	async run(msg, args){
-		let {wsid} = args
+		let {wsid, force} = args
 
 		let r = /\D/
 		while (wsid.match(r)){
 			wsid = wsid.replace(r, '')
 		}
 
-		await this.queue('UpdateWorkshop', {wsid})
+		await this.queue('UpdateWorkshop', {wsid, force})
 		return msg.say(`Added ${wsid} to the update queue.\nCheck <#611492538674839553> for status changes.`)
 	}
 }
