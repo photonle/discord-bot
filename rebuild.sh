cb473fdfe01fa0ed74cfe6fb479c3b95bd7350a7
John Internet <jonjon1234.github@gmail.com>
2019-09-26 07:53:40 +0100

    Removed outdated rebuild script.

--- rebuild.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-declare -r DIR="$(realpath $(dirname "${BASH_SOURCE[0]}"))"
-declare -r INST="$( basename "$DIR" )"
-
-docker stop "$INST"
-docker rm "$INST"
-docker build -t internet/"$INST" "$DIR"
-docker run -d --name "$INST" --mount source=photonbot,target=/app --restart always internet/"$INST"
