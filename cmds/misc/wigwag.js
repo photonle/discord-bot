d02ddcc50cf3f80cc96c0cfba20f040a7f91dcc8
GermanDude-ger <59023102+GermanDude-ger@users.noreply.github.com>
2019-12-22 17:37:44 +0100

    Update wigwag.js

--- cmds/misc/wigwag.js
+++ cmds/misc/wigwag.js
@@ -7,23 +7,10 @@ module.exports = class SupportCommand extends Command {
 			group: 'util',
 			memberName: 'wigwag',
 			description: 'Do you do wigwags properly?',
-			args: [{
-				key: 'wigwag',
-				label: 'wigwag',
-				prompt: 'wigwag',
-				type: 'string',
-				default: 'photon'
-			}]
 		})
 	}
 
 	async run(msg, args, _){
-		switch (args.wigwag){
-			default:
-			case 'photon':
 				return msg.say(`Seems like you don't know how to wigwag properly. What a shame. Here, take this: <https://docs.google.com/document/d/1ksWNVhNDi_NaUy1kZOu5aD8wsGJQaC4RCO-_ZZfy14w/edit> `)
-			case 'bot':
-				return msg.say(`Seems like you don't know how to wigwag properly. What a shame. Here, take this: <https://docs.google.com/document/d/1ksWNVhNDi_NaUy1kZOu5aD8wsGJQaC4RCO-_ZZfy14w/edit> `)
-		}
 	}
 }
